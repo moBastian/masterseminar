e2522b6f855f30d7788cae90acab31f01e56f967
morten <stu114712@yahoo.de>
2018-01-20 18:08:04 +0100

    Mailer?

--- test/mailers/previews/student_mailer_preview.rb
+++ test/mailers/previews/student_mailer_preview.rb
@@ -1,4 +1,7 @@
 # Preview all emails at http://localhost:3000/rails/mailers/student_mailer
 class StudentMailerPreview < ActionMailer::Preview
+  def sample_mail_preview
+    StudentMailer.edited("test@test.com", "12kjk2j1k")
+  end
 
 end
